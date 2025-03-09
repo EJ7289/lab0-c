32b2525e72a86f4b2220f216acf9e645b388c5e8
Evelina <evelinajhang@gmail.com>
2025-03-09 12:54:15 +0800

    Use fast&slow pointer to implement 'q_delete_mid'
    To delete the middle node, follow these steps:
    1. Check if the head is empty or NULL; return false if it is.
    2. Define tail since the list is circular.
    3. Use a indirect pointer ('**indir') to simplify the code.
    4. Use fast and slow pointers in a loop to locate the middle
    node.
    5. Call 'list_del()' to unlink the node, then free both the
    node and its value.
    
    Change-Id: I997092f48545f362fedb5d525af48b187f234e69

--- queue.c
+++ queue.c
@@ -134,6 +134,25 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return false;
+
+    // Find the middle point
+    struct list_head **indir = &head->next;
+    const struct list_head *tail = head->prev;
+    for (const struct list_head *fast = head->next;
+         fast != tail && fast->next != tail; fast = fast->next->next) {
+        indir = &(*indir)->next;
+    }
+
+    // Remove middle point from the queue
+    struct list_head *tmp = *indir;
+    list_del(tmp);
+
+    // Delete the middle point
+    element_t *elem = list_entry(tmp, element_t, list);
+    free(elem->value);
+    free(elem);
     return true;
 }
 
